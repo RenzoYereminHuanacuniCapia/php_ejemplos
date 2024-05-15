f156f56c7398c2531770234b1c3f22e7d2add3e6
jose <jkqr2016@gmail.com>
2024-05-15 17:57:18 -0500

    bucle

---
+++ 07-bucles.php
@@ -0,0 +1,28 @@
+<?php
+    echo "Ingrese su Notas :
+    $nota = fgets(STDIN);
+    $contador=1;
+    $sumanotas=0;
+    while($contador<=3) {
+        $sumanotas = $sumanotas + $nota;
+        echo "Ingrese su Notas: ";
+        $nota = fgets(STDIN);
+        $contador++; //$contador=$contador+1
+}
+$notapromedio = $sumanotas/3;
+echo "\n El promedio de notas es ".$notapromedio;
+
+
+
+
+<?php 
+
+    //logica que me permite dar el mayor de 5 numeros
+    $mayor=0;
+    for($i=1;$i<=5;$i++) {
+        echo "Ingrese un numero : ; $numero= fgets(STDIN); if ($mayor<$numero) {}
+        $mayor-$numero;
+        echo "El numero mayor es :".$mayor;
+
+    }
+}
