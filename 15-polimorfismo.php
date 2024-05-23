3a8da84ea3d550ac2c228ccce2225b1a4de50283
RenzoYereminHuanacuniCapia <1487987@senati.pe>
2024-05-23 16:26:45 -0500

    Update 15-polimorfismo.php

--- 15-polimorfismo.php
+++ 15-polimorfismo.php
@@ -20,4 +20,18 @@ $rectangulo = new Rectangulo($b,$a);
 $area=$rectangulo->calcularArea();
 echo "EL AREA DEL RECTANGULO ES ".$area;
 
+echo "AREA DE CUADRADO\n";
+echo "INGRESE EL LADO :";
+$l = trim(fgets(STDIN));
+$cuadrado = new Cuadrado($l);
+$areaCuadrado = $cuadrado->calcularArea();
+echo "EL AREA DEL CUADRADO ES " . $areaCuadrado . "\n";
+
+echo "AREA DE CIRCULO\n";
+echo "INGRESE EL RADIO :";
+$r = trim(fgets(STDIN));
+$circulo = new Circulo($r);
+$areaCirculo = $circulo->calcularArea();
+echo "EL AREA DEL CIRCULO ES " . $areaCirculo . "\n";
+
 ?>
