8e22f92e55310dab0d057fdac32a336a230e3f29
jose <jkqr2016@gmail.com>
2024-05-23 17:07:57 -0500

    variables

---
+++ 02-variables.php
@@ -0,0 +1,10 @@
+<?php
+
+$nombres ="Renzo Yeremin";
+$apellidos="Huanacuni Capia";
+$curso="BACKEND DEVELOPER WEB";
+$semestre="TERCER SEMESTRE";
+
+echo $nombres." ".$apellidos." ".$curso." ".$semestre;
+
+?>
