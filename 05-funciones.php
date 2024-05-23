5df279ec6ffa616a174dd3a8cee583835dc422d0
jose <jkqr2016@gmail.com>
2024-05-23 16:38:02 -0500

    funciones

---
+++ 05-funciones.php
@@ -0,0 +1,29 @@
+<?php
+
+function obtenerSaludo($parametro) {
+    return "Hola como estas ".$parametro;
+}
+
+function mostrarDatos($nombres,$apellidos,$curso,$semestre) {
+    $mensaje = "Hola soy ".$nombres." ".$apellidos
+    ." y estoy en el curso ".$curso." del ".$semestre;
+    return $mensaje;
+}
+
+function calcular($operador,$numero1,$numero2) {
+    if($operador=="+")
+        return $numero1+$numero2;
+    if($operador=="-")
+        return $numero1-$numero2;
+    if($operador=="*")
+        return $numero1*$numero2;
+    if($operador=="/")
+        return $numero1/$numero2;
+}
+
+ echo obtenerSaludo("renzo")."\n";
+ echo mostrarDatos("renzo","huanacuni",
+ "backend developer web","tercer semestre")."\n";
+ echo "El total de la suma es : ".calcular("+",10,34);
+ 
+?>
