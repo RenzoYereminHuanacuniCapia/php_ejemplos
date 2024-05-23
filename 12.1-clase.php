8dfeb9a5b38b9fe4ff1ef129fa53de8e4b8d3dea
jose <jkqr2016@gmail.com>
2024-05-23 17:49:03 -0500

    encapsulamiento.php

---
+++ 12.1-clase.php
@@ -0,0 +1,42 @@
+<?php
+
+   class Calculadora {
+
+        public $primerNumero;
+        public $segundoNumero;
+
+        function __construct($num1 = 0, $num2 = 0) {         
+            $this->primerNumero=$num1;
+            $this->segundoNumero=$num2;
+        }
+
+        public function sumar() {
+            return $this->primerNumero+$this->segundoNumero;
+        }
+
+        public function restar() {
+            return $this->primerNumero-$this->segundoNumero;
+        }
+
+        public function multiplicar() {
+            return $this->primerNumero*$this->segundoNumero;
+        }
+
+        public function dividir() {
+            if($this->segundoNumero==0) return "No es divisible";
+            else return $this->primerNumero/$this->segundoNumero;
+        }
+
+        public function potencia() {
+            // echo "primerNumero:".$this->primerNumero."\n";
+            // echo "segundoNumero:".$this->segundoNumero."\n";
+            return pow($this->primerNumero,$this->segundoNumero);
+        }
+
+        public function raiz() {
+            return sqrt($this->primerNumero);
+        }
+
+    }
+
+?>
